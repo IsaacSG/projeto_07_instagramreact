f230c783c1b5b431274bfb3411480249e13efcc4
IsaacSG <isaacgraciano123@gmail.com>
2022-05-06 02:38:17 -0300

    Navbar On

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import ReactDOM from 'react-dom';
+import Navbar from './navbar';
+
+ReactDOM.render(<Navbar /> ,document.querySelector(".root"));
---
+++ src/stories.js
@@ -0,0 +1,5 @@
+function Stories(props){
+    return (
+        
+    );
+}
